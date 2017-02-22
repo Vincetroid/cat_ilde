ea4620a2e684b785c6684bf6a13f84173d335c97
Vincetroid <vince_trance@hotmail.com>
2017-02-21 19:52:40 -0600

    Remover 'Capturado por' con su función
    Modificar botón 'Información de libro' por 'Reseña'

--- ajax/detalles_publicacion.php
+++ ajax/detalles_publicacion.php
@@ -1,40 +1,10 @@
 <?php
 
-
-//NO LLEGAN LOS DATOS ACA EN LINEA
-// echo " <script>
-// 			alert('".$_POST['id_aut']."');
-// 		</script>
-// 		";
-// echo " <script>
-// 			alert('".$_POST['id_lib']."');
-// 		</script>
-// 		";
-// //CLICK A LIBRO
+//CLICK A LIBRO
 if(isset($_POST['id_lib']) === true && empty($_POST['id_lib']) === false && isset($_POST['id_aut']) === true && empty($_POST['id_aut']) === false){
 	
 	//Conexion a bd yendo atrás hacia carpeta raíz
 	require('../conexion.php');
-
-	// $req = require('../conexion.php');
-	// if($req){
-	// 	echo " <script>
-	// 		alert('Success2');
-	// 	</script>
-	// 	";
-	// } else {
-	// 	echo " <script>
-	// 		alert('Bad2');
-	// 	</script>
-	// 	";
-	// }
-
-	function get_usuario_capturador($id_usu_cap){
-	   $consulta_capturador = "SELECT id_usu,nombre_usu FROM usuario WHERE id_usu='$id_usu_cap';";
-	   $resultado_capturador = mysql_query($consulta_capturador) or die (mysql_error());
-	   $fila = mysql_fetch_array($resultado_capturador);
-	   return $fila['nombre_usu'];
-	}
 
 	//Quitar letras de id
 	$idAut = preg_replace("/[^0-9]/","",$_POST['id_aut']);
@@ -53,8 +23,7 @@ if(isset($_POST['id_lib']) === true && empty($_POST['id_lib']) === false && isse
 		<tr><td>Lugar de publicación </td><td>'. $fila['lugar_pub'] . '</td></tr>
 		<tr><td>Páginas </td><td>'. $fila['pags'] . '</td></tr>
 		<tr><td>Portada </td><td id="nombre_img_port">'. $fila['portada'] . '</td></tr>
-		<tr><td>Capturado por </td><td>'. get_usuario_capturador($fila['id_usu']). '</td></tr>
-		<tr><td colspan="2"><center><input class="info-libro" type="button" value="Información de libro"/></center></td></tr>';
+		<tr><td colspan="2"><center><input class="info-libro" type="button" value="Reseña"/></center></td></tr>';
 
 	//Liberar consulta
     mysql_free_result($resultado_libro);
